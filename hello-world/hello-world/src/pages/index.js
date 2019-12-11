adad22ad6ea88e679c2bdcaf0f767ddc4749b835
juani11 <juanitagliero@gmail.com>
2019-12-10 22:47:22 -0300

    grid 3 columnas y fotos de pruba
    Paso la grilla a 3 columnas, y agrego imagenes de prueba

--- hello-world/hello-world/src/pages/index.js
+++ hello-world/hello-world/src/pages/index.js
@@ -5,100 +5,44 @@ import { chunk } from 'lodash';
 
 const Index = () => {
 
-  const data = useStaticQuery(graphql`
-  query {
-    allFile(
-      filter: {
-        extension: { regex: "/(jpg)|(png)|(jpeg)/" }
-        relativeDirectory: { eq: "io/ioLarge" }
-      }
-    ) {
-      edges {
-        node {
-          base
-          childImageSharp {
-            fluid(maxWidth: 1000, quality: 100) {
-              ...GatsbyImageSharpFluid_tracedSVG
-            }
-          }
-        }
-      }
-    }
-  }
-`)
+	const data = useStaticQuery(graphql`
+		query {
+			allFile(filter: {extension: {regex: "/(jpg)|(png)|(jpeg)/"} relativeDirectory: {eq:"prueba"}}) {
+				edges {
+					node {
+						base
+						childImageSharp {
+							fluid(maxWidth: 800) {
+								...GatsbyImageSharpFluid
+							}
+							id
+						}
+					}
+				}
+			}
+		}
+	`)
 
 
 
-  const columns = chunk(data.allFile.edges.map(image => ({
-    ...image.node.childImageSharp.fluid
-  })), 7);
-
-  console.log(columns);
-
-  return (
-    <div className="row">
-      {columns.map(column => {
-        return (
-          <div className="column">
-            {column.map(image =>
-              <Img fluid={image} 
-              style={{
-                marginTop: "8px",
-                verticalAlign: "middle",
-                width:"100%"
-              }}/>
-            )}
-          </div>)
-      })}
-    </div>
-  );
+	const columns = chunk(data.allFile.edges.map(image => ({
+		...image.node.childImageSharp
+	})), 4);
+	return (
+		<div className="container">
+			{columns.map(column => {
+				return (
+					<div className="column">
+						{column.map(image =>
+							<div className="img-wrapper" key={image.id} >
+								<Img fluid={image.fluid} backgroundColor={"rgb(0, 0, 0,0.7)"} />
+							</div>
+						)}
+					</div>
+				)
+			})}
+		</div>
+	);
 }
 
 export default Index;
-/*
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "io/ioSlide/Atrapasueños.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 2976) {
-      # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
-      ...GatsbyImageSharpFluid_noBase64
-    }
-      }
-    }
-  }
-`
-export const data = useStaticQuery(graphql`
-  query {
-    allFile(
-      filter: {
-        extension: { regex: "/(jpg)|(png)|(jpeg)/" }
-        relativeDirectory: { eq: "io/ioLarge" }
-      }
-    ) {
-      edges {
-        node {
-          base
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`)
-
-export default ({ data }) => (
-  data.allFile.edges.map(image => (
-    <Img
-      fluid={image.node.childImageSharp.fluid}
-      alt={image.node.base.split(".")[0]} // only use section of the file extension with the filename
-    />
-  ))
-)
-
-*/
