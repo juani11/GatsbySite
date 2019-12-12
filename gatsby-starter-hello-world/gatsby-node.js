be78542b4847b867b1846d6343bd3517ffb067d4
juani11 <juanitagliero@gmail.com>
2019-12-12 13:43:37 -0300

    implementacion de createPages en gatsby-node.js
    Agrego exports.createPages en gatsby-node.js . Para generar paginas dinamicamente por cada imagen de la grilla

---
+++ gatsby-starter-hello-world/gatsby-node.js
@@ -0,0 +1,30 @@
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}}) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+  `)
+  result.data.allFile.edges.forEach(({ node }) => {
+    const finalSlug = '/' + (node.name);
+    createPage({
+      path: finalSlug,
+      component: path.resolve(`./src/templates/detail.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: finalSlug
+      },
+    })
+  })
+}
--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,53 +1,13 @@
 import React from "react"
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
-import { chunk } from 'lodash';
+
 import '../styles/styles.css'
+import Ilustrations from "../components/Ilustrations";
 
 const Index = () => {
 
-    const staticQ = useStaticQuery(graphql`
-        query {
-            allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}}) {
-                edges {
-                    node {
-                        childImageSharp {
-                            fluid(background:"rgb(240, 196, 80)"){
-                                ...GatsbyImageSharpFluid
-                            }
-                        }
-                        relativePath
-                    }
-                }
-            }
-        }    
-    `);
 
-    console.log(staticQ);
-    const columns = chunk(
-        staticQ.allFile.edges.map(image => ({
-            ...image.node.childImageSharp.fluid
-        })), 3);
-
-    console.log(columns);
     return (
-        <div className="container">
-            <div className="qztBA">
-            {columns.map(column => {
-                return (
-                    <div className="column">
-                        {column.map(image => {
-                            return (
-                                <div className="imgIl">
-                                    <Img fluid={image} backgroundColor={"rgb(240, 196, 80)"}></Img>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            })}
-            </div>
-        </div>
+        <Ilustrations/>
     );
 }
 
