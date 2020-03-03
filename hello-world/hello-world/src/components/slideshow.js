b69efbb1544f91e3410ef322a8e2537f0a002efd
juani11 <juanitagliero@gmail.com>
2020-03-02 21:10:14 -0300

    Agrego componente Slideshow
    componente Slideshow el cual tiene el slider automatico de imagenes.  Va estar contenido dentro del Layout

---
+++ hello-world/hello-world/src/components/slideshow.js
@@ -0,0 +1,121 @@
+import React, { } from 'react';
+import { useStaticQuery, graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
+
+import './slideshow.css'
+
+function SlideShow() {
+
+    const { allFile } = useStaticQuery(
+        graphql`
+            query {
+                allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}  relativeDirectory: {eq:"io/ioSlide"}}) {
+                    edges {
+                        node {
+                            id
+                            name
+                            childImageSharp {
+                                fluid(maxWidth:1200,maxHeight:900){
+                                    ...GatsbyImageSharpFluid
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    const fullBG = {
+        "width": "100%",
+        "height": "100vh",
+        "overflow": "hidden",
+        "display": "flex",
+
+    }
+
+    return (
+        <div id="slideshowbg" style={fullBG} >
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[1].node.childImageSharp.fluid}
+                    title={allFile.edges[1].node.name}
+                    id={allFile.edges[1].node.id}
+                    role="img"
+                    aria-label={allFile.edges[1].node.name}
+                    preserveStackingContext={true}
+
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[2].node.childImageSharp.fluid}
+                    title={allFile.edges[2].node.name}
+                    id={allFile.edges[2].node.id}
+                    role="img"
+                    aria-label={allFile.edges[2].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[3].node.childImageSharp.fluid}
+                    title={allFile.edges[3].node.name}
+                    id={allFile.edges[3].node.id}
+                    role="img"
+                    aria-label={allFile.edges[3].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[4].node.childImageSharp.fluid}
+                    title={allFile.edges[4].node.name}
+                    id={allFile.edges[4].node.id}
+                    role="img"
+                    aria-label={allFile.edges[4].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[5].node.childImageSharp.fluid}
+                    title={allFile.edges[5].node.name}
+                    id={allFile.edges[5].node.id}
+                    role="img"
+                    aria-label={allFile.edges[5].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[6].node.childImageSharp.fluid}
+                    title={allFile.edges[6].node.name}
+                    id={allFile.edges[6].node.id}
+                    role="img"
+                    aria-label={allFile.edges[6].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+            <li>
+                <BackgroundImage
+                    Tag="section"
+                    fluid={allFile.edges[7].node.childImageSharp.fluid}
+                    title={allFile.edges[7].node.name}
+                    id={allFile.edges[7].node.id}
+                    role="img"
+                    aria-label={allFile.edges[7].node.name}
+                    preserveStackingContext={true}
+                />
+            </li>
+        </div>
+    )
+}
+
+export default SlideShow
