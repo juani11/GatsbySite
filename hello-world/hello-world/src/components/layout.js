d43cf86e77e880c7c65149b2bde207bde3b5d57e
juani11 <juanitagliero@gmail.com>
2020-03-09 11:58:35 -0300

    Update layout.js
    Si la pagina a renderizar es el index o /bio, el Layout contendra Slideshow,Navbar y Footer.
    Si la pagina a renderizar es el detalle de una ilustracion, el Layout contendra unicamente el Footer

--- hello-world/hello-world/src/components/layout.js
+++ hello-world/hello-world/src/components/layout.js
@@ -2,12 +2,40 @@ import React from "react"
 import "./layout.css"
 import Navbar from "./navbar"
 import Footer from "./footer";
+import PageTransition from 'gatsby-plugin-page-transitions';
+import '../styles/bootstrap.min.css'
+import '../styles/bootstrap-grid.min.css'
+import SlideShow from "./slideshow";
+import Transition from "./transition";
+import { Helmet } from "react-helmet";
 
-export default ({ children }) => (
-    <div>
-        {/*<Navbar />*/}
-        {children}
-        <Footer />
-    </div>
-)
+export default ({ children, location }) => {
+    if ((location.pathname === '/') || (location.pathname === '/bio')) {
+        return (
+            <div>
+                <Helmet>
+                    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+                </Helmet>
+                <SlideShow />
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <Helmet>
+                    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+                </Helmet>
+                {children}
+                <Footer />
+
+            </div>
+        )
+    }
+
+
+}
 
