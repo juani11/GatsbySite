8dac73fc9b27db594324be33aa1f5e23d9bcf7f7
juani11 <juanitagliero@gmail.com>
2020-03-09 12:04:53 -0300

    Update detail.js
    Agrego hook useRef() para tener la referencia al componenete y hacer scroll al mismo  al momento de renderizarlo.
    Agrego Magnifier para hacer zoom a la imagen y verla con mas detalle

--- hello-world/hello-world/src/templates/detail.js
+++ hello-world/hello-world/src/templates/detail.js
@@ -1,29 +1,56 @@
-import React from "react"
+import React,{useRef,useEffect} from "react"
 import { graphql } from "gatsby"
-import IlustrationsGrid from "../components/IlustrationsGrid";
 import Img from "gatsby-image"
 import "./detail.css"
-import Footer from "../components/footer";
+
+import { Magnifier, MagnifierContainer, MOUSE_ACTIVATION, TOUCH_ACTIVATION } from 'react-image-magnifiers'
+import PageTransition from 'gatsby-plugin-page-transitions';
+
+
+const scrollToRef = (ref) => window.scrollTo({ behavior: "auto",top: ref.current.offsetTop})   
 
 export default ({ data }) => {
 
     const ilustration = data.selectIlustration.edges[0].node
 
-    const detailStyle={
+    
+    const detailStyle = {
         textAlign: "center",
-        fontFamily:"raleway"
+        fontFamily: "raleway"
     }
 
+    const refDetail = useRef(null)
+
+    useEffect(() => {
+        scrollToRef(refDetail)
+    }, []);
+
+
     return (
-        <div>
-            <h3 style={detailStyle}>{ilustration.name}</h3>
-            <div className="img-detail-wrapper">
-                <Img fluid={ilustration.childImageSharp.fluid} backgroundColor={"rgb(0, 0, 0,0.7)"} durationFadeIn={1200} />
+
+        <div  ref={refDetail}>
+            <div className="detailBrand" style={{ textAlign: "center" }}>
+                <img src="layoutImgs/logob.png" width="200" height="42" alt="Maria Julia Tagliero" />
             </div>
-            <h2 style={detailStyle}>OTRAS ILUSTRACIONES</h2>
-            <IlustrationsGrid data={data.othersIlustrations}/>
-            <Footer/>
+            <PageTransition transitionTime={450}>
+                <div className="img-detail-wrapper">
+                    {/*<Img fluid={ilustration.childImageSharp.fluid} durationFadeIn={1200} />*/}
+                    <MagnifierContainer title="Side By Side Magnifier" >
+                        <Magnifier
+                            className="input-position"
+                            imageSrc={`io/ioMedium/20.jpg`}
+                            largeImageSrc={`io/ioLarge/20.jpg`}
+                            mouseActivation={MOUSE_ACTIVATION.SINGLE_CLICK} // Optional
+                            touchActivation={TOUCH_ACTIVATION.DOUBLE_TAP} // Optional
+                        />
+                    </MagnifierContainer>
+                </div>
+            </PageTransition>
+            {/*
+            <h5 style={detailStyle}> OTHERS ILUSTRATIONS</h5>
+            <IlustrationsGrid data={data.othersIlustrations}/>*/}
         </div>
+
 
     )
 }
@@ -36,7 +63,7 @@ export const query = graphql`
                     name
                     base
 						childImageSharp {
-							fluid(maxWidth: 800) {
+							fluid{
 								...GatsbyImageSharpFluid
 								originalName
 							}
